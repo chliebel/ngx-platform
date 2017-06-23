bfe7f7c30e6522d24f49ee1687ad91ff6a5d0a61
Manuel Rauber <manuel.rauber@thinktecture.com>
2017-06-23 15:54:18 +0200

    feat(build): Reworked build system

--- webpack.config.js
+++ webpack.config.js
@@ -3,27 +3,27 @@
 const path = require('path');
 
 module.exports = {
-    devtool: 'cheap-source-map',
-    entry: path.resolve('src/index.ts'),
-    output: {
-        filename: 'ngx-platform.umd.js',
-        path: path.resolve('dist'),
-        library: '@ngx-unicorns/ngx-platform',
-        libraryTarget: 'umd',
-    },
-    resolve: {
-        extensions: ['.ts', '.js'],
-    },
-    externals: [/^@angular\//, /^rxjs\//],
-    module: {
-        rules: [
-            {
-                test: /\.ts$/,
-                loader: 'ts-loader',
-                options: {
-                    silent: true
-                }
-            }
-        ]
-    }
+  devtool: 'cheap-source-map',
+  entry: path.resolve('src/index.ts'),
+  output: {
+    filename: 'dist/ngx-platform.umd.js',
+    path: path.resolve(__dirname),
+    library: '@ngx-unicorns/ngx-platform',
+    libraryTarget: 'umd',
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  externals: [/^@angular\//, /^rxjs\//],
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        loader: 'ts-loader',
+        options: {
+          silent: true
+        }
+      }
+    ]
+  }
 };
